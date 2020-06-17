fa124da3c5fd6a9a278d796667f04a34f3b8d744
Raffi Dilanchian <raffidilanchian@gmail.com>
2020-06-17 13:11:49 +0430

    Fix title font weight

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,8 @@ module.exports = {
     }
   },
   variants: {
-    textAlign: ['responsive', 'direction']
+    textAlign: ['responsive', 'direction'],
+    fontWeight: ['responsive', 'direction']
   },
   plugins: [require('tailwindcss-dir')()]
 };
