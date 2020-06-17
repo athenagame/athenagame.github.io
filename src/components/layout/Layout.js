73821e1578579165f6a25fc849b4d07559f2e67b
Samim Pezeshki <psamim@gmail.com>
2020-06-17 11:57:49 +0430

    Add some SEO

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Footer from './Footer';
 import Header from './Header';
+import t from '../../data/languages';
 
 const Layout = ({ children, lang }) => {
   return (
@@ -26,6 +27,20 @@ const Layout = ({ children, lang }) => {
   _gs('GSN-639569-C');
                 `}
         </script>
+        <script type="application/ld+json">
+          {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "url": "https://athenagameshow.github.io",
+          "name": "Athena Game Show Maker",
+        }
+      `}
+        </script>
+        <meta content={t.secondaryHeadline.en} property="og:description" />
+        <meta name="description" content={t.secondaryHeadline.en} />
+        <meta property="og:title" content="Athena Game Show Maker" />
+        <title>Athena Game Show Maker</title>
         <body dir={lang === 'fa' ? 'rtl' : 'ltr'} />
       </Helmet>
       <Header lang={lang} />
