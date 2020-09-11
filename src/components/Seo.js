d318fbbdcd759dc81a8f6f7abfb129fba59b445c
Samim Pezeshki <psamim@gmail.com>
2020-09-11 23:53:19 +0430

    Add favicon

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -39,7 +39,20 @@ const SEO = ({ title, description, image, article }) => {
   };
 
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+    <Helmet
+      title={seo.title}
+      titleTemplate={titleTemplate}
+      meta={[
+        {
+          name: `description`,
+          content: seo.description
+        },
+        {
+          name: 'keywords',
+          content: 'athena,game,show'
+        }
+      ]}
+    >
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
 
@@ -73,9 +86,15 @@ const SEO = ({ title, description, image, article }) => {
         }
       `}
       </script>
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   );
 };
 
 export default SEO;
-
